442019c56e950446ad69f04ae84649e16ebe4380
Bruno Garcia <brunogarcia84@gmail.com>
2023-03-08 01:07:48 +0200

    chore: Changing minor issues with server and package.json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import express from "express";
-import dotenv from "dotenv";
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
+let PORT = process.env.PORT || 5000;
 
 // Config dotenv
 dotenv.config();
@@ -16,4 +17,4 @@ app.use(express.urlencoded({ extended: true }));
 // Setting the static folder
 app.use(express.static("public"));
 
-export default app;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
